acc86ba2134dc2fd654089340d6db1e82dd34abd
Om Singh <94895043+omsinghthakur@users.noreply.github.com>
2023-05-22 23:21:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function(nums, target) {  
+function threeSum (nums, target) {  
   nums.sort((a, b) => a - b);
 	let ans=nums[0]+nums[1]+nums[2]; 
-  for (let i = 0; i < nums.length - 2; i++) {
+  for (let i = 0; i < nums.length - 2; i++) { 
     let left = i + 1;
     let right = nums.length - 1; 
     while (left < right) {
